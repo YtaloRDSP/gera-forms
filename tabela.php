566027fd69e6fe0c8b76565431a9a5adb9b6fc7d
Ytalo Ribeiro <70536739+YtaloRDSP@users.noreply.github.com>
2021-05-02 15:28:16 -0400

    Falha de autenticação no banco de dados

--- tabela.php
+++ tabela.php
@@ -36,11 +36,6 @@
             </thead>
             <tbody>";
 
-    $servername = 'localhost';
-    $username = 'root';
-    $password = '';
-    $database = 'sixsigma';
-
     try {
         $conn = new PDO("mysql:host=$servername", $username, $password);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
