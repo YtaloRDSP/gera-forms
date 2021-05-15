9eec7be6e266f12af27100e2942147c06a0e7b8f
Ytalo Ribeiro <70536739+YtaloRDSP@users.noreply.github.com>
2021-05-15 13:59:14 -0400

    Update self-destroy.php

--- self-destroy.php
+++ self-destroy.php
@@ -15,7 +15,7 @@
     $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $sql = "DROP TABLE Beneficiários";
+    $sql = "DROP TABLE Beneficiarios";
     $conn->exec($sql);
 
     echo "Cadastros Destruídos";
