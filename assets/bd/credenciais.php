1803b202a2acc7d39e541e901d4ee3bfe02774b5
Ytalo Ribeiro <70536739+YtaloRDSP@users.noreply.github.com>
2021-06-15 19:24:31 -0400

    Alteração de credencial

--- assets/bd/credenciais.php
+++ assets/bd/credenciais.php
@@ -1,14 +1,14 @@
 <?php
-    // $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-    // $servername = $url["host"];
-    // $username = $url["user"];
-    // $password = $url["pass"];
-    // // //$database = 'heroku_1d16e51af604e58';
+    $servername = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $database = 'heroku_1d16e51af604e58';
     // $database = 'heroku_8c940033eab5791';//(teste)
 
-    $servername = 'localhost';
-    $username = 'root';
-    $password = '';
-    $database = 'sixsigma';
+//     $servername = 'localhost';
+//     $username = 'root';
+//     $password = '';
+//     $database = 'sixsigma';
 ?>
